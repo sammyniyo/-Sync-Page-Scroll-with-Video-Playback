75e92c606168f2a2b488f0a4fe9a6136fdfc4488
sammyniyo <samshakul@gmail.com>
2024-05-07 17:41:48 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import { useRef } from "react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { useCallback, useMemo, useRef } from "react";
+
 function App() {
   const ref = useRef<HTMLCanvasElement>(null);
-  return (
-  <div style={{height: '5000px'}}>
-  <canvas ref={ref}></canvas>;
-  </div>
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['center end', 'start start'],
+  });
+
+  const images = useMemo(() => {
+    const loadedImages: HTMLImageElement[] = [];
+    for (let i = 1; i <= 86; i++) {
+      const img = new Image();
+      img.src = `/images/${i}.webp`;
+      loadedImages.push(img);
+    }
+    return loadedImages;
+  }, []);
+
+
+  const render = useCallback(
+    (index: number) => {
+      if (images[index - 1]) {
+        ref.current?.getContext('2d')?.drawImage(images[index - 1], 0, 0);
+      }
+    },
+    [images]
   );
-  
+
+  useMotionValueEvent(scrollYProgress, 'change', (latest) => {
+    render(Number(latest.toFixed()));
+  });
+
+  return (
+    <div style={{
+      height: '6000px',
+      backgroundColor: 'black',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <div style={{ height: '2000px' }} />
+      <canvas ref={ref}></canvas> 
+    </div>
+  );
 }
 
 export default App;
